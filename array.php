826c2fe4db026f7fd8e08add16ff1c080a9df91b
dell <umathesupriya@gmail.com>
2018-12-15 13:30:44 +0530

    this php coding of an array

---
+++ array.php
@@ -0,0 +1,200 @@
+<?php 
+
+$arr=array(
+
+	   array(4,5,6),
+	   array('hello', 'hii','bye'),
+	   array(4.2,3.6,7.2)
+
+	 );
+	 
+	 print_r($arr); 
+
+	 echo "<br>";
+
+
+$arr="my name is khan";
+
+print_r(explode(" ", $arr));
+
+
+
+
+echo "<br>";
+
+
+
+$arr=array('abhi','car', 'hello');
+sort($arr);
+echo "<pre>";
+
+print_r($arr);
+
+
+
+echo "<br>";
+
+
+
+$arr=array('ali', 'car','devil','zebra');
+rsort($arr);
+echo "<pre>";
+print_r($arr);
+
+
+
+
+echo "<br>";
+
+
+
+
+
+$arr=array('aman'=>23,'suppie'=>34,'piu'=>56);
+ksort($arr);
+echo "<pre>";
+print_r($arr);
+
+
+echo "<br>";
+
+
+$arr=array('aman'=>23,'suppie'=>34,'piu'=>56);
+arsort($arr);
+echo "<pre>";
+print_r($arr);
+
+
+
+
+echo "<br>";
+
+$arr=array('aman'=>23,'suppie'=>34,'piu'=>56);
+krsort($arr);
+echo "<pre>";
+print_r($arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
