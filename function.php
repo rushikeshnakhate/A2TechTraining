55362b753122823a7f879fb1eb88ef8830f33573
dell <umathesupriya@gmail.com>
2018-12-15 13:07:56 +0530

    This is php coding of functions.

---
+++ function.php
@@ -0,0 +1,79 @@
+
+
+
+
+<?php 	
+
+
+
+
+function sum()
+{
+
+	echo "hello i m suppie<br>";
+
+}
+
+sum();
+sum();
+sum();
+
+
+function sub ($x,$y)
+{
+
+	$z=$x-$y;
+	echo $z;
+
+	echo "<br>";
+}function mul($x,$y)
+{
+
+
+	$z=$x*$y;
+	echo $z;
+    echo "<br>";
+}function dev($x,$y)
+{
+
+
+	$z=$x/$y;
+	echo $z;
+
+	echo "<br>";
+
+}function add($x,$y)
+{
+
+
+	$z=$x+$y;
+	echo $z;
+     echo "<br>";
+}
+
+sub(8,9); 
+mul(6,4);
+dev(9,3);
+add(6,8);
+
+
+
+
+
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
