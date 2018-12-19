fdc7f529e03b0bb1b4b78bf57041e2a29d4832c8
shrawani rangari <shrawanirangari29@gmail.com>
2018-12-19 12:56:20 +0530

    here is the php file used by  'if else & for cindition'

---
+++ shrawani_file.php
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+<?php
+$a="welcome";
+$b="hello";
+
+echo $a.$b;
+
+echo "<br>";
+
+
+$x=5;
+if ($x%2==0)
+{
+    echo "no  is even.";
+}
+ else
+  {
+	echo "no is odd.";
+}
+
+echo "<br>";
+
+$s=1;
+
+for ($i=1; $i<=5; $i++)
+ {
+	echo "*";
+}
+
+ ?>
