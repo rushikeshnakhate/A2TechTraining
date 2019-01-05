675fe5f731ca5a59e25478b433b612e460495a99
nikita@903_4074 <nikitarewatkar22@gmail.com>
2019-01-05 14:24:09 +0530

    this is the coding of login form of php file

---
+++ nikita_login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User registration system using PHP amd MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <div class="header">
+     	<h2>Login</h2>
+     </div>
+
+     <form method="post" action="login.php">
+     	<div class="input-group">
+     	 	<label>Username</label>
+     	 	<input type="text" name="Username" >
+     	    </div>
+
+     	<div class="input-group">
+     	 	<label>Password</label>
+     	 	<input type="text" name="Password_1" >
+     	    </div>
+
+     	<div class="input-group">
+     		 <button type="Submit" name="login" class="btn">Login</button>
+     	</div>   
+     	<p>
+     		Not yet a member? <a href="register.php">Sign up</a>
+     	</p> 
+
+     </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
